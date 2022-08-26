0e5c9d1aff251629c100ce4f96b899c53dae6745
kranthi kiran <imkranthik@gmail.com>
2022-08-26 17:03:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "ec2" {
   ami                       = "ami-052efd3df9dad4825"
   instance_type             = "t2.micro"
   key_name                  = "instance-ping"
-  security_group_ids        = ["sg-087c4b0ae917d8b67"]
+  security_group_ids        = ["sg-03c33213d1559e4a4"]
   subnet_id                 = "subnet-00f114f5d0ca36190"
     name                    = "testinstance"
     env                     = "test"
