4dc7acda823c8aa37e174ced6a7c09e1e39697df
kranthi kiran <imkranthik@gmail.com>
2022-08-26 16:53:24 +0530

    ec2

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ module "ec2" {
   source  = "app.terraform.io/kranthikiran/ec2/aws"
   version = "1.0.1"
   # insert required variables here
-  region                    = us-east-1
-  ami                       = ami-052efd3df9dad4825
-  instance_type             = t2.micro
-  key_name                  = instance-ping
-  vpc_security_group_ids    = sg-087c4b0ae917d8b67
-  subnet_id                 = subnet-00f114f5d0ca36190
+  region                    = "us-east-1"
+  ami                       = "ami-052efd3df9dad4825"
+  instance_type             = "t2.micro"
+  key_name                  = "instance-ping"
+  vpc_security_group_ids    = "sg-087c4b0ae917d8b67"
+  subnet_id                 = "subnet-00f114f5d0ca36190"
 tags                        = {
-    Name                    = testinstance
-    env                     = test
+    Name                    = "testinstance"
+    env                     = "test"
   }
 }
